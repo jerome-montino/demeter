671e898957f2a75d0a5fbfaef47fa3d5f00daf1a
Jerome Montino <jerome.montino@gmail.com>
2019-08-06 19:13:02 +0800

    Added file upload and permission granting.

--- demeter.py
+++ demeter.py
@@ -11,6 +11,7 @@ class Demeter(object):
         self.credentials = self._authorize(credentials, client_secret)
         self.service = self._build_service()
         self.files = self.service.files()
+        self.permissions = self.service.permissions()
 
     def _authorize(self, credentials, client_secret):
         store = file.Storage(credentials)
@@ -40,3 +41,43 @@ class Demeter(object):
         while done is False:
             status, done = downloader.next_chunk()
         return export_path
+
+    def upload_file(self, file_name, file_path, mime_type, team_drive=False, team_drive_id=None):
+        file_metadata = {'name': file_name}
+        media = http.MediaFileUpload(file_path, mimetype=mime_type)
+        fl = None
+
+        if not team_drive:
+            fl = self.files.create(body=file_metadata, media_body=media, fields='id').execute()
+        elif team_drive and team_drive_id is not None:
+            payload = {
+                'parents': [team_drive_id],
+                'name': file_name
+            }
+            fl = self.files.create(supportsAllDrives=True, media_body=media, body=payload).execute()
+        elif team_drive and team_drive_id is None:
+            raise ValueError("team_drive_id should have value if this is a team drive upload")
+
+        return fl
+
+    def give_permission(self, file_id, permission, team_drive=False):
+        pr = self.permissions.create(supportsAllDrives=team_drive, fileId=file_id, body=permission, fields='id').execute()
+        return pr
+
+    def give_domain_permission(self, file_id, role, domain, team_drive=False):
+        domain_permission = {
+            'type': 'domain',
+            'role': role,
+            'domain': domain
+        }
+        pr = self.give_permission(file_id, domain_permission, team_drive=team_drive)
+        return pr
+
+    def give_user_permission(self, file_id, role, email_address, team_drive=False):
+        user_permission = {
+            'type': 'user',
+            'role': role,
+            'email_address': email_address
+        }
+        pr = self.give_permission(file_id, user_permission, team_drive=team_drive)
+        return pr
